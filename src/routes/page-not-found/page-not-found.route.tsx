f421a9501291d0482a7d17ae7e27b95c7c100a07
samstha07 <samshrestha85@gmail.com>
2023-01-12 23:41:32 +0545

    ADDED: head title in page not found

--- src/routes/page-not-found/page-not-found.route.tsx
+++ src/routes/page-not-found/page-not-found.route.tsx
@@ -1,11 +1,18 @@
+import { Helmet } from 'react-helmet';
+
 const PageNotFound = () => {
   return (
-    <div className='flex mt-40 justify-center items-center'>
-      <div className='mt-5 flex items-center'>
-        <span className='text-2xl'>404 | </span>
-        <span className='text-base ml-2'>Page not found</span>
+    <>
+      <Helmet>
+        <title>Not Found</title>
+      </Helmet>
+      <div className='flex mt-40 justify-center items-center'>
+        <div className='mt-5 flex items-center'>
+          <span className='text-2xl'>404 | </span>
+          <span className='text-base ml-2'>Page not found</span>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
