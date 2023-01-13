4499c386f6e6c210ceb6142f8a6974e71701ff8b
samstha07 <samshrestha85@gmail.com>
2023-01-13 22:35:44 +0545

    CHANGED: folder name

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import Header from 'components/header/header.component';
 import ErrorBoundary from 'layout/error-bounday.layout';
+import AppRoute from 'pages/app.route';
 import { Toaster } from 'react-hot-toast';
-import AppRoute from 'routes/app.route';
 
 function App() {
   return (
